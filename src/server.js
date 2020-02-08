2966161b4531fbbe0da4056b181b8fe518d34911
Violeta Libertad <morada.libertad@gmail.com>
2020-02-08 11:57:11 -0300

    first base commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+export default app;
