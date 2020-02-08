f7e1537c4cd26813fdafa833e344cc5e3fef10b2
Violeta Libertad <morada.libertad@gmail.com>
2020-02-08 12:11:16 -0300

    server up

---
+++ src/controllers/usersController.js
@@ -0,0 +1,7 @@
+const list = (req, res) => {
+  res.send({ message: 'Soy el controlador del usuario' });
+};
+
+export default {
+  list,
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes/index';
 
 const app = express();
 
